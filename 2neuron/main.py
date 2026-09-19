8b1a9f3c6735b8fa4536062e6c9442e55b511c82
tiborwn <mb4dev@gmail.com>
2026-09-19 21:01:57 +0200

    2 neuron model finished - calculate if temperature is comfortable or not

---
+++ 2neuron/main.py
@@ -0,0 +1,102 @@
+import json, math
+
+parameters = json.load(open('parameters.json'))
+
+trainingData = [
+    # temperature, comfortable (0 - uncomfortable, 1 - comfortable)
+
+    (0, 0),
+    (5, 0),
+    (10, 0),
+    (12, 0),
+
+    (15, 1),
+    (18, 1),
+    (20, 1),
+    (22, 1),
+    (25, 1),
+
+    (28, 0),
+    (30, 0),
+    (32, 0),
+    (35, 0),
+    (40, 0),
+]
+
+learningRate = 0.01
+trainingMode = True
+
+def neuron(i, inputToken):
+    neuronWeight = parameters['hiddenLayer'][i]['weight']
+    neuronBias = parameters['hiddenLayer'][i]['bias']
+    computed = neuronWeight * inputToken + neuronBias
+    return max(0, computed)
+
+def output(inputToken):
+    outputToken = 0
+    outputBias = parameters['outputLayer']['bias']
+    for i in range(0, 2):
+        relu = neuron(i, inputToken)
+        outputWeight = parameters['outputLayer']['weights'][i]
+        outputToken += outputWeight * relu
+    outputToken += outputBias
+    predictionSigmoid = 1 / (1 + math.e ** -outputToken)
+    return predictionSigmoid
+
+def calculateLoss(trainingData):
+    averageLoss = 0
+    for temperature, target in trainingData:
+        averageLoss += (output(temperature) - target) ** 2
+    averageLoss /= len(trainingData)
+    return averageLoss
+
+def reluDerivative(num):
+    return 1 if num > 0 else 0
+
+if output(int(input('Temperature: '))) > 0.5:
+    print('comfortable')
+else: 
+    print('uncomfortable')
+
+if trainingMode:
+    lossBefore = calculateLoss(trainingData)
+    print("LOSS BEFORE: ", lossBefore)
+    
+    for i in range(0, 10):
+        for temperature, target in trainingData:
+            outputToken = 0
+            outputBias = parameters['outputLayer']['bias']
+
+            for i in range(0, 2):
+                relu = neuron(i, temperature)
+                outputWeight = parameters['outputLayer']['weights'][i]
+                outputToken += outputWeight * relu
+            outputToken += outputBias
+
+            predictionSigmoid = 1 / (1 + math.e ** -outputToken)
+
+            for i in range(0, 2):
+                outputWeight = parameters['outputLayer']['weights'][i]
+                weight = parameters['hiddenLayer'][i]['weight']
+                bias = parameters['hiddenLayer'][i]['bias']
+                raw = weight * temperature + bias
+                relu = neuron(i, temperature)
+
+                weightGradient = (2 * (predictionSigmoid - target) * (predictionSigmoid * (1 - predictionSigmoid)) * outputWeight * reluDerivative(raw) * temperature)
+                biasGradient = (2 * (predictionSigmoid - target) * (predictionSigmoid * (1 - predictionSigmoid)) * outputWeight * reluDerivative(raw))
+
+                parameters['hiddenLayer'][i]['weight'] = parameters['hiddenLayer'][i]['weight'] - (learningRate * weightGradient)
+                parameters['hiddenLayer'][i]['bias'] = parameters['hiddenLayer'][i]['bias'] - (learningRate * biasGradient)
+                parameters['outputLayer']['weights'][i] = parameters['outputLayer']['weights'][i] - (learningRate * (( (2 * (predictionSigmoid - target)) * (predictionSigmoid * (1 - predictionSigmoid)) ) * relu))
+
+            parameters['outputLayer']['bias'] = parameters['outputLayer']['bias'] - (learningRate * ( (2 * (predictionSigmoid - target)) * (predictionSigmoid * (1 - predictionSigmoid)) ))
+
+    lossAfter = calculateLoss(trainingData)
+    print("LOSS AFTER: ", lossAfter)
+
+    if lossAfter < lossBefore:
+        print("OK TRAINING")
+        with open("parameters.json", "w", encoding="utf-8") as file:
+            json.dump(parameters, file, indent=2, ensure_ascii=False)
+    else:
+        print("WRONG TRAINING")
