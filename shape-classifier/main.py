e0db6ff14b78cbb58172dbe424840ec3fed271be
Tibor <149430469+tiborwn@users.noreply.github.com>
2026-09-21 00:09:33 +0200

    turn image into tokens

--- shape-classifier/main.py
+++ shape-classifier/main.py
@@ -1,5 +1,11 @@
 from PIL import Image
 import numpy as np
 
-inputImage = Image.open('input.png')
-print(np.array(inputImage))
+inputImage = Image.open('input.png').convert('RGB')
+inputImageTokens = []
+for line in np.array(inputImage):
+    for i in range(0, len(line)):
+        if np.all(line[i] == [255, 255, 255]):
+            inputImageTokens.append(1)
+        else:
+            inputImageTokens.append(0)
